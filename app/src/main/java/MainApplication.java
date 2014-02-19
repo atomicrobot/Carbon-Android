bb8af72b3bb1b0902c2c199d903628698a880d4d
Patrick Hammond <patrick@madebyatomicrobot.com>
2014-02-18 21:54:52 -0500

    Adding Crashlytics startup to the default project.

--- app/src/main/java/MainApplication.java
+++ app/src/main/java/MainApplication.java
@@ -1,11 +1,21 @@
 package PACKAGE_NAME;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
 
 public class MainApplication extends Application {
     
     @Override
     public void onCreate() {
     	super.onCreate();
+
+        setupCrashReporter();
+    }
+
+    private void setupCrashReporter() {
+        //noinspection ConstantConditions,PointlessBooleanExpression
+        if (!BuildConfig.DEBUG) {
+            Crashlytics.start(this);
+        }
     }
 }
