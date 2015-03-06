a178bffa1cec74932b809151320ff8536b696349
Patrick Hammond <patrick@madebyatomicrobot.com>
2015-03-05 23:30:58 -0500

    Butterknife sample.

--- app/src/main/java/com/mycompany/myapp/ui/fragments/MainFragment.java
+++ app/src/main/java/com/mycompany/myapp/ui/fragments/MainFragment.java
@@ -5,16 +5,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.mycompany.myapp.BuildConfig;
 import com.mycompany.myapp.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import hugo.weaving.DebugLog;
 
 public class MainFragment extends Fragment {
 
+    @InjectView(R.id.version)
+    TextView versionView;
+
+    @InjectView(R.id.fingerprint)
+    TextView fingerprintView;
+
     @DebugLog
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.inject(this, view);
+        return view;
+    }
+
+    @DebugLog
+    @Override
+    public void onResume() {
+        super.onResume();
+        versionView.setText(String.format("Version: %s", BuildConfig.VERSION_NAME));
+        fingerprintView.setText(String.format("Fingerprint: %s", BuildConfig.VERSION_FINGERPRINT));
+    }
+
+    @DebugLog
+    @Override
+    public void onDestroyView() {
+        ButterKnife.reset(this);
+        super.onDestroyView();
     }
 }
