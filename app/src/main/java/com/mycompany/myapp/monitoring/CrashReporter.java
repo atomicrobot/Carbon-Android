86338a5e523a8093cd0fca1009f7abc415267ece
Patrick Hammond <patrick@madebyatomicrobot.com>
2015-03-06 23:31:58 -0500

    Fixing flagged PMD code.

--- app/src/main/java/com/mycompany/myapp/monitoring/CrashReporter.java
+++ app/src/main/java/com/mycompany/myapp/monitoring/CrashReporter.java
@@ -1,6 +1,6 @@
 package com.mycompany.myapp.monitoring;
 
 public interface CrashReporter {
-    public void logMessage(String message);
-    public void logException(String message, Exception ex);
+    void logMessage(String message);
+    void logException(String message, Exception ex);
 }
