5109b01c415956c35833e8f405f0c6789bb9f269
Patrick Hammond <patrick@madebyatomicrobot.com>
2015-03-07 13:26:02 -0500

    Rise and shine.

--- app/src/debug/java/com/mycompany/myapp/app/ApplicationInitialization.java
+++ app/src/debug/java/com/mycompany/myapp/app/ApplicationInitialization.java
@@ -1,5 +1,11 @@
 package com.mycompany.myapp.app;
 
+import android.app.Activity;
+import android.app.Application.ActivityLifecycleCallbacks;
+import android.os.Bundle;
+
+import com.mycompany.myapp.ui.RiseAndShine;
+
 public class ApplicationInitialization {
     private final MainApplication application;
 
@@ -8,6 +14,41 @@ public class ApplicationInitialization {
     }
 
     public void immediateInitialization() {
+        application.registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                RiseAndShine.riseAndShine(activity);
+            }
 
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 }
---
+++ app/src/debug/java/com/mycompany/myapp/ui/RiseAndShine.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp.ui;
+
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.app.KeyguardManager.KeyguardLock;
+import android.content.Context;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.view.WindowManager;
+
+/**
+ * Inspired from https://gist.github.com/JakeWharton/f50f3b4d87e57d8e96e9
+ */
+@SuppressWarnings("deprecated")
+public class RiseAndShine {
+    private static final int LOCK_FLAGS = PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE;
+
+    public static void riseAndShine(Activity activity) {
+        KeyguardManager keyguardManager = (KeyguardManager) activity.getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardLock keyguardLock = keyguardManager.newKeyguardLock(activity.getLocalClassName());
+        keyguardLock.disableKeyguard();
+
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+
+        PowerManager powerManager = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
+        WakeLock lock = powerManager.newWakeLock(LOCK_FLAGS, "wakeup!");
+
+        lock.acquire();
+        lock.release();
+    }
+}
