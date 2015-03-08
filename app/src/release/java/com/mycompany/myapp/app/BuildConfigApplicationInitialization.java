07397a214a5e81261522808bbf7f0d1f067f3aa1
Patrick Hammond <patrick@madebyatomicrobot.com>
2015-03-08 00:41:11 -0500

    Cleaning up code.

--- app/src/release/java/com/mycompany/myapp/app/BuildConfigApplicationInitialization.java
+++ app/src/release/java/com/mycompany/myapp/app/BuildConfigApplicationInitialization.java
@@ -2,10 +2,10 @@ package com.mycompany.myapp.app;
 
 import com.crashlytics.android.Crashlytics;
 
-public class ApplicationInitialization {
+public class BuildConfigApplicationInitialization {
     private final MainApplication application;
 
-    public ApplicationInitialization(MainApplication application) {
+    public BuildConfigApplicationInitialization(MainApplication application) {
         this.application = application;
     }
 
