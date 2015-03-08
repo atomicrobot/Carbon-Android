090faee18083cce329a1ad30220015a72f3aed47
Patrick Hammond <patrick@madebyatomicrobot.com>
2015-03-07 22:50:53 -0500

    PMD warning.

--- app/src/main/java/com/mycompany/myapp/ui/ActivityScope.java
+++ app/src/main/java/com/mycompany/myapp/ui/ActivityScope.java
@@ -4,7 +4,6 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import javax.inject.Qualifier;
 import javax.inject.Scope;
 
 @Scope
