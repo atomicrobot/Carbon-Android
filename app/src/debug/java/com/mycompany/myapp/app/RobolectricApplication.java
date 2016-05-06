981fe8e4778ea408ccce4383712a094fc689a96a
Patrick Hammond <patrick@madebyatomicrobot.com>
2016-05-06 09:44:42 -0400

    Adding in Multidex by default.

--- app/src/debug/java/com/mycompany/myapp/app/RobolectricApplication.java
+++ app/src/debug/java/com/mycompany/myapp/app/RobolectricApplication.java
@@ -1,13 +1,7 @@
 package com.mycompany.myapp.app;
 
 import android.app.Application;
-import android.content.Context;
-import android.support.multidex.MultiDex;
 
 public class RobolectricApplication extends Application {
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
+
 }
