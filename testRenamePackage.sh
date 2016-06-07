607f0ea85be10c5ed2cd8c08c6db69c5d75a46c0
Patrick Hammond <patrick@madebyatomicrobot.com>
2016-06-07 00:40:24 -0400

    Trying not to delete stuff that hasn't been pushed...

--- testRenamePackage.sh
+++ testRenamePackage.sh
@@ -4,6 +4,6 @@ cd ..
 rm -rf android-starter-project-copy
 cp -R android-starter-project android-starter-project-copy
 cd android-starter-project-copy
-./renamePackage.sh demoapp com.demo.mobile
+./renamePackage.sh com.demo.mobile
 grep -IR 'myapp' . | grep gradle
 find . -name 'myapp' | grep gradle
