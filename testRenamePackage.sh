2865491b4e2ad3191a59ed6c23c0bb3c5c5002d2
Stuart Bowman <stuartsoft2012@gmail.com>
2017-02-26 15:57:04 -0500

    updated testRenamePackage.sh

--- testRenamePackage.sh
+++ testRenamePackage.sh
@@ -4,6 +4,6 @@ cd ..
 rm -rf android-starter-project-copy
 cp -R android-starter-project android-starter-project-copy
 cd android-starter-project-copy
-./renamePackage.sh com.demo.mobile
+python renamePackage.py com.demo.mobile
 grep -IR 'myapp' . | grep gradle
 find . -name 'myapp' | grep gradle
