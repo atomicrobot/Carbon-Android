952b1ad48c1e2c8ad573335786737fb0ea2c0f38
Trent Hudepohl <trenthudy@gmail.com>
2018-05-01 10:36:56 -0400

    get author avatar from GitHub api

--- app/src/main/java/com/mycompany/myapp/data/api/github/model/Commit.kt
+++ app/src/main/java/com/mycompany/myapp/data/api/github/model/Commit.kt
@@ -3,18 +3,16 @@ package com.mycompany.myapp.data.api.github.model
 import com.google.gson.annotations.SerializedName
 
 data class Author(
-        @SerializedName("name") val name: String)
+        @SerializedName("login") val name: String,
+        @SerializedName("avatar_url") val avatarUrl: String)
 
 data class CommitDetails(
-        @SerializedName("message") val message: String,
-        @SerializedName("author") val author: Author)
+        @SerializedName("message") val message: String)
 
 data class Commit(
-        @SerializedName("commit") val commit: CommitDetails) {
+        @SerializedName("commit") val commit: CommitDetails,
+        @SerializedName("author") val author: Author) {
 
     val commitMessage: String
         get() = commit.message
-
-    val author: String
-        get() = commit.author.name
 }
