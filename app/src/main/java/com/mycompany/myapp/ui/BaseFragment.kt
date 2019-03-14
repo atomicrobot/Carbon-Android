c1fe86aa9cb412d0a65e6a498e6a2b705d43c074
akhilamadari <akhilamadari.developer@gmail.com>
2019-03-14 08:43:12 -0400

    migrating to Androidx

--- app/src/main/java/com/mycompany/myapp/ui/BaseFragment.kt
+++ app/src/main/java/com/mycompany/myapp/ui/BaseFragment.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.fragment.app.Fragment
 import kotlin.reflect.KClass
 
-abstract class BaseFragment: androidx.fragment.app.Fragment() {
+abstract class BaseFragment: Fragment() {
     fun <VM: ViewModel> getViewModel(viewModelClass: KClass<VM>): VM {
         return ViewModelProviders.of(activity!!).get(viewModelClass.java)
     }
