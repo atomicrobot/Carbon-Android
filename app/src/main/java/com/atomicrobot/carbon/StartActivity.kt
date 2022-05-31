efdb60b4e55e9e0a5efc4b772a773f71bdd6a06c
matthewdauterman <matthew.dauterman@atomicrobot.com>
2022-05-31 16:24:18 -0400

    passed DeepLinkInteractorTest.kt tests

--- app/src/main/java/com/atomicrobot/carbon/StartActivity.kt
+++ app/src/main/java/com/atomicrobot/carbon/StartActivity.kt
@@ -1,14 +1,40 @@
 package com.atomicrobot.carbon
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.atomicrobot.carbon.ui.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import com.atomicrobot.carbon.ui.splash.SplashViewModel
+import timber.log.Timber
 
 @AndroidEntryPoint
 class StartActivity : BaseActivity() {
+    private val splashViewModel: SplashViewModel by viewModels()
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_start)
+
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val appLinkAction = intent.action
+        val appLinkData: Uri? = intent.data
+        if (Intent.ACTION_VIEW == appLinkAction) {
+            appLinkData?.encodedPath?.also {
+                //these are koin functions what is the equivalent for hilt?
+                splashViewModel.setDeepLinkUri(appLinkData)
+                splashViewModel.setDeepLinkPath(appLinkData.encodedPath)
+
+                Timber.d("appLinkData = $appLinkData")
+                Timber.d("appLinkData.encodedPath = ${appLinkData.encodedPath}")
+                Timber.d("appLinkData.query = ${appLinkData.query}")
+                Timber.d("appLinkData.encodedQuery = ${appLinkData.encodedQuery}")
+                Timber.d("appLinkData.queryParameterNames = ${appLinkData.queryParameterNames}")
+            }
+        }
     }
 }
