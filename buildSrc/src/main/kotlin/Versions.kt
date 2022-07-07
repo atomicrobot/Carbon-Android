a8bcaee4bdbdd7a8bd8cc662bed109624972acd3
matthewdauterman <matthew.dauterman@atomicrobot.com>
2022-07-07 10:31:53 -0400

    wip kts conversion commiting for snapshot before file type conversion

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,39 @@
+object Versions {
+    //Dependency Versions
+    const val appCompatVersion = "1.4.1"
+    const val composeHiltNavigationVersion = "1.0.0"
+    const val constraintLayoutVersion = "2.1.2"
+    const val coreVersion = "1.7.0"
+    const val gradlePlugin = "4.2.1"
+    const val kotlin = "1.5.0"
+    const val leakCanaryVersion = "2.7"
+    const val lifecycleRuntimeVersion = "2.4.0"
+    const val lifecycleVersion = "2.0.0"
+    const val materialVersion = "1.4.0"
+    const val moshiVersion = "1.13.0"
+    const val navVersion = "2.4.2"
+    const val okHttpVersion = "4.9.1"
+    const val playServicesVersion = "17.6.0"
+    const val retrofitVersion = "2.9.0"
+    const val recyclerViewVersion = "1.2.1"
+    const val supportVersion = "1.0.0"
+    const val timberVersion = "5.0.1"
+
+}
+
+object TestVersions {
+    //Test Dependency Versions
+    const val androidTestSupportVersion = "1.4.0"
+    const val daggermockVersion = "0.8.4"
+    const val esspressoVersion = "3.3.0"
+    const val junitTestVersion = "1.1.3"
+    const val junitVersion = "4.13.2"
+    const val mockitoKotlinVersion = "1.6.0"
+    const val mockitoVersion = "4.1.0"
+    const val roboelectricVersion = "4.7.3"
+}
+
+object Carbon{
+    const val firebase_bom_version = "28.0.0"
+
+}
