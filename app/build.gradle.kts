70680c31c26c7edfd2ec48410763c77f151cff1a
matthewdauterman <matthew.dauterman@atomicrobot.com>
2022-07-26 10:50:31 -0400

    kts conversion complete pending review

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
-import org.codehaus.groovy.runtime.ArrayTypeUtils.dimension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 buildscript {
     repositories {
@@ -18,75 +17,72 @@ plugins {
     id("dagger.hilt.android.plugin")
     id("com.google.firebase.crashlytics")
     id("jacoco")
+    id("com.google.gms.google-services")
 }
+
 val appVersion = "1.0"
 val minSdkVersion = 21
 val targetSdkVersion = 32
 val compileSdkVersion = 32
-//TODO placeholder values
-//var versionCode : Int
-//if(project.hasProperty("buildNumber")){
-//    versionCode = Integer.parseInt(buildNumber)
-//}
-//else{
-//    versionCode = 1
-//}
-val versionCode = 1
-val versionFingerprint = "\"DEV\""
-val versionName = "$appVersion b${versionCode}"
+var versionCode = 1
+if(project.hasProperty("buildNumber")) {
+    val buildNumber = Integer.parseInt(project.property("buildNumber").toString())
+    versionCode = buildNumber
+}
+val version = versionCode
+val versionFingerprint =
+    if (project.hasProperty("fingerprint")) "\"" + project.property("fingerprint").toString() + "\"" else "\"DEV\""
 
 android {
-//    compileOptions {
-//        sourceCompatibility JavaVersion.VERSION_11
-//        targetCompatibility JavaVersion.VERSION_11
-//    }
-//
-//    kotlinOptions {
-//        jvmTarget = ("11")
-//    }
-//
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 
-//
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     defaultConfig {
         applicationId= "com.atomicrobot.carbon"
 
         minSdk = minSdkVersion
         targetSdk = targetSdkVersion
         compileSdk = 32
-//        multiDexEnabled(true)
+        multiDexEnabled = true
 
-        versionCode = versionCode
+        versionCode = version
+        versionName = "\"$appVersion b$version\""
 
         buildConfigField("String", "VERSION_FINGERPRINT", versionFingerprint)
-//        buildConfigField("String", "VERSION_NAME", "$versionName")
+        buildConfigField("String", "VERSION_NAME", versionName!!)
 
-//        proguardFile getDefaultProguardFile("proguard-android.txt")
-//        proguardFile ("proguard-rules.pro")
-//
-//        testInstrumentationRunner ("com.atomicrobot.carbon.CustomAppTestRunner")
+        proguardFiles("proguard-android.txt", "proguard-rules.pro")
+        testInstrumentationRunner = "com.atomicrobot.carbon.CustomAppTestRunner"
+
     }
-//
-//    signingConfigs {
-//        // If you are creating signing keys, consider setting up Google Play App Signing!
-//        // See: https://developer.android.com/studio/publish/app-signing.html#google-play-app-signing
-//        release {
-//            apply from: rootProject.file("distribution/keys/sample.gradle")
-//
-//            storeFile rootProject.file(sampleKeystore)
-//            storePassword sampleKeystorePassword
-//            keyAlias sampleKeyAlias
-//            keyPassword sampleKeyPassword
-//        }
-//        // Use debug.keystore in this project so that debug version works with AR"s Carbon web link
-//        // setup. You can safely remove this section if you are not using web linking within your app
-//        debug {
-//            storeFile rootProject.file("distribution/keys/debug.keystore")
-//            keyAlias ("androiddebugkey")
-//            keyPassword ("android")
-//            storePassword ("android")
-//        }
-//    }
-//
+
+    signingConfigs {
+        // If you are creating signing keys, consider setting up Google Play App Signing!
+        // See: https://developer.android.com/studio/publish/app-signing.html#google-play-app-signing
+        create("release") {
+            apply(rootProject.file("distribution/keys/sample.gradle"))
+            storeFile = rootProject.file("sampleKeystore")
+            storePassword = "sampleKeystorePassword"
+            keyAlias = "sampleKeyAlias"
+            keyPassword = "sampleKeyPassword"
+        }
+        // Use debug.keystore in this project so that debug version works with AR"s Carbon web link
+        // setup. You can safely remove this section if you are not using web linking within your app
+        getByName("debug") {
+            storeFile = rootProject.file("distribution/keys/debug.keystore")
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+            storePassword = "android"
+        }
+    }
+
+
     flavorDimensions("app")
     productFlavors {
         create("dev") {
@@ -98,21 +94,21 @@ android {
             applicationId = "com.atomicrobot.carbon"
         }
     }
-//
-//    buildTypes {
-//        debug {
-//            minifyEnabled false
-//            shrinkResources false
-//            testCoverageEnabled true
-//        }
-//
-//        release {
-//            minifyEnabled true
-//            shrinkResources true
-//            signingConfig signingConfigs.release
-//        }
-//    }
-//
+    buildTypes {
+        getByName("debug") {
+           isMinifyEnabled = false
+           isShrinkResources = false
+           isTestCoverageEnabled = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isTestCoverageEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
     buildFeatures {
         dataBinding = true
         compose = true
@@ -121,33 +117,33 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.1.1"
     }
-//    packagingOptions {
-//        resources {
-//            excludes += ("/META-INF/{AL2.0,LGPL2.1}")
-//        }
-//    }
+    packagingOptions {
+        resources {
+            excludes += ("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
     namespace = "com.atomicrobot.carbon"
-//    testOptions {
-//        animationsDisabled true
-//
-//        unitTests {
-//            includeAndroidResources = true
-//        }
-//    }
+    testOptions {
+        animationsDisabled = true
+
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
 }
 
-//kapt {
-//    correctErrorTypes = true
-//    generateStubs = true
-//}
+kapt {
+    correctErrorTypes = true
+    generateStubs = true
+}
 
 
 // App dependency versions
 val appCompatVersion = "1.4.1"
-val compose_act_version = "1.4.0"
-val compose_foundation_version = "1.2.0-beta02"
-val compose_version = "1.1.1"
-val compose_vm_version = "2.4.1"
+val composeActVersion = "1.4.0"
+val composeFoundationVersion = "1.2.0-beta02"
+val composeVersion = "1.1.1"
+val composeVmVersion = "2.4.1"
 val supportVersion = "1.0.0"
 val playServicesVersion = "17.6.0"
 val lifecycleVersion = "2.0.0"
@@ -175,18 +171,18 @@ val junitTestVersion = "1.1.3"
 val daggerMockVersion = "0.8.4"
 
 /*
-TODO brought in from build.gradle (carbon-android)
-Figure out what to do with these
+Brought in from build.gradle (carbon-android)
  */
-val firebase_bom_version = "28.0.0"
-val kotlin_version = "1.6.10"
-val hilt_version = "2.40.1"
-val navigation_version = "1.0.0"
+val firebaseBomVersion = "28.0.0"
+val kotlinVersion = "1.6.10"
+val hiltVersion = "2.40.1"
+val navigationVersion = "1.0.0"
+val jacocoVersion = "0.8.6"
 
 
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:${firebase_bom_version}"))
+    implementation(platform("com.google.firebase:firebase-bom:${firebaseBomVersion}"))
     implementation ("com.google.firebase:firebase-analytics-ktx")
 
     // Add the Firebase Crashlytics SDK.
@@ -195,8 +191,8 @@ dependencies {
     // Recommended: Add the Google Analytics SDK.
     implementation ("com.google.firebase:firebase-analytics:$rootProject.ext.firebase_analytics_version")
 
-
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    implementation ("org.jacoco:org.jacoco.core:$jacocoVersion")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
     implementation ("androidx.test.ext:junit-ktx:1.1.3")
 //    kapt ("androidx.databinding:databinding-compiler:$rootProject.ext.android_plugin_version")
     // Need this because of Kotlin
@@ -219,33 +215,42 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleRuntimeVersion")
 
     // Integration with activities
-    implementation ("androidx.activity:activity-compose:$compose_act_version")
+    implementation ("androidx.activity:activity-compose:$composeActVersion")
     // Compose Material Design
-    implementation ("androidx.compose.material:material:$compose_version")
+    implementation ("androidx.compose.material:material:$composeVersion")
     // Animations
-    implementation ("androidx.compose.animation:animation:$compose_version")
+    implementation ("androidx.compose.animation:animation:$composeVersion")
     // Tooling support (Previews, etc.)
-    implementation ("androidx.compose.ui:ui-tooling:$compose_version")
+    implementation ("androidx.compose.ui:ui-tooling:$composeVersion")
     // Integration with ViewModels
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:$compose_vm_version")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:$composeVmVersion")
     // UI Tests
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$compose_version")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$composeVersion")
 
-    implementation ("androidx.compose.foundation:foundation:$compose_foundation_version")
+    implementation ("androidx.compose.foundation:foundation:$composeFoundationVersion")
+
+    //App architecture - Dagger core
+    implementation("com.google.dagger:dagger:$hiltVersion")
+    kapt("com.google.dagger:dagger-compiler:$hiltVersion")
+
+    //Dagger -android
+    api("com.google.dagger:dagger-android:$hiltVersion")
+    api("com.google.dagger:dagger-android-support:$hiltVersion")
+    kapt("com.google.dagger:dagger-android-processor:$hiltVersion")
 
     // App architecture - Hilt
-    implementation ("com.google.dagger:hilt-android:$hilt_version")
-    kapt ("com.google.dagger:hilt-android-compiler:$hilt_version")
-    androidTestImplementation ("com.google.dagger:hilt-android-testing:$hilt_version")
-    kaptAndroidTest ("com.google.dagger:hilt-android-compiler:$hilt_version")
+    implementation ("com.google.dagger:hilt-android:$hiltVersion")
+    kapt ("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    androidTestImplementation ("com.google.dagger:hilt-android-testing:$hiltVersion")
+    kaptAndroidTest ("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
     // JSON
     implementation ("com.squareup.moshi:moshi-kotlin:$moshiVersion")
     kapt ("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
 
     // Navigation
-    implementation ("android.arch.navigation:navigation-fragment-ktx:$navigation_version")
-    implementation ("android.arch.navigation:navigation-ui-ktx:$navigation_version")
+    implementation ("android.arch.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation ("android.arch.navigation:navigation-ui-ktx:$navigationVersion")
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation ("androidx.hilt:hilt-navigation-compose:$composeHiltNavigationVersion")
 
@@ -288,88 +293,128 @@ dependencies {
     androidTestImplementation ("com.github.fabioCollini:DaggerMock:$daggerMockVersion")
 }
 
-//jacoco {
-//    toolVersion = rootProject.ext.jacoco_version
-//}
-//
-//tasks.withType(Test) {
-//    jacoco.includeNoLocationClasses = true
-//    jacoco.excludes = ["jdk.internal.*"]
-//}
-//
-//task jacocoTestReport(type: JacocoReport, dependsOn: ["testDevDebugUnitTest", "createDevDebugCoverageReport"]) {
-//    reports {
-//        xml.enabled = true
-//        html.enabled = true
-//    }
-//
-//    def excludes = [
-//            "**/R.class",
-//            '**/R$*.class',
-//            "**/BuildConfig.*",
-//            "**/Manifest*.*",
-//            "**/*Test*.*",
-//            "android/**/*.*",
-//            /* Parcelize */
-//            "**/*Creator.*",
-//            /* Data binding */
-//            "**/*Binding*.*",
-//            "**/BR.**",
-//            /* Dagger */
-//            "**/*_MembersInjector.*",
-//            "**/*_Factory.*",
-//            "**/*_*Factory.*",
-//            "**/Dagger*Component*.*",
-//            "**/Dagger*Subcomponent*.*",
-//            "**/devsettings/**/*.*"
-//    ]
-//
-//    getClassDirectories().setFrom(fileTree(
-//            // Java generated classes on Android project (debug build)
-//            dir: "$buildDir/intermediates/classes/dev/debug",
-//            excludes: excludes
-//    ) + fileTree(
-//            // Kotlin generated classes on Android project (debug build)
-//            dir: "$buildDir/tmp/kotlin-classes/devDebug",
-//            excludes: excludes
-//    ))
-//
-//    getSourceDirectories().setFrom(files([
-//            "src/main/java",
-//            "src/main/kotlin"
-//    ]))
-//
-//    getExecutionData().setFrom(fileTree(dir: project.buildDir, includes: [
-//            "jacoco/testDevDebugUnitTest.exec",
-//            "outputs/code-coverage/connected/**/*coverage.ec"
-//    ]))
-//}
-//
-//// Kotlin plugin for testing
-//allOpen {
-//    annotation("dagger.Module")
-//    annotation("com.atomicrobot.carbon.Mockable")
-//}
-//apply plugin: ("com.google.gms.google-services")
-//
-//ktlint {
-//    debug = true
-//    android = true
-//    outputToConsole = true
-//    outputColorName = ("RED")
-//    disabledRules = ["final-newline"]
-//    reporters {
-//        reporter ("plain")
-//        reporter ("checkstyle")
-//        reporter ("sarif")
-//    }
-//    kotlinScriptAdditionalPaths {
-//        include fileTree("scripts/")
-//    }
-//    filter {
-//        exclude("**/generated/**")
-//        include("**/kotlin/**")
-//    }
-//}
+jacoco {
+    toolVersion = jacocoVersion
+}
+
+tasks.withType<Test> {
+    configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+
+    }
+}
+//would not build as a private val
+val fileFilter = mutableSetOf(
+    "**/R.class",
+    "**/R\$*.class",
+    "**/BuildConfig.*",
+    "**/Manifest*.*",
+    "**/*Test*.*",
+    "android/**/*.*",
+    /* Parcelize */
+    "**/*Creator.*",
+    /* Data binding */
+    "**/*Binding*.*",
+    "**/BR.**",
+    /* Dagger */
+    "**/*_MembersInjector.*",
+    "**/*_Factory.*",
+    "**/*_*Factory.*",
+    "**/Dagger*Component*.*",
+    "**/Dagger*Subcomponent*.*",
+    "**/devsettings/**/*.*"
+)
+private val classDirectoriesTree = fileTree(project.buildDir) {
+    include(
+        ""
+    )
+    exclude(fileFilter)
+}
+
+private val sourceDirectoriesTree = fileTree("${project.buildDir}") {
+    include(
+        "src/main/java/**",
+        "src/main/kotlin/**"
+    )
+}
+private val executionDataTree = fileTree(project.buildDir) {
+    include(
+
+    )
+}
+fun JacocoReportsContainer.reports() {
+    xml.required.set(true)
+    html.required.set(true)
+}
+fun JacocoCoverageVerification.setDirectories() {
+    sourceDirectories.setFrom(sourceDirectoriesTree)
+    classDirectories.setFrom(classDirectoriesTree)
+    executionData.setFrom(executionDataTree)
+}
+fun JacocoReport.setDirectories() {
+    sourceDirectories.setFrom(sourceDirectoriesTree)
+    classDirectories.setFrom(classDirectoriesTree)
+    executionData.setFrom(executionDataTree)
+}
 
 
+if (tasks.findByName("jacocoTestReport") == null) {
+
+    tasks.register<JacocoReport>("jacocoTestReport") {
+        description = "Code coverage report for both Android and Unit tests."
+        dependsOn("testDebugUnitTest", "createDebugCoverageReport")
+        reports {
+            reports()
+        }
+        setDirectories()
+    }
+}
+if (tasks.findByName("jacocoAndroidCoverageVerification") == null) {
+    tasks.register<JacocoCoverageVerification>("jacocoAndroidCoverageVerification") {
+        description = "Code coverage verification for Android both Android and Unit tests."
+        dependsOn("testDebugUnitTest", "createDebugCoverageReport")
+        violationRules {
+            rule {
+                limit {
+                    counter = "INSTRUCTIONAL"
+                    value = "COVEREDRATIO"
+                    minimum = "0.5".toBigDecimal()
+                }
+            }
+        }
+        setDirectories()
+    }
+}
+
+// Kotlin plugin for testing
+allOpen {
+    annotation("dagger.Module")
+    annotation("com.atomicrobot.carbon.Mockable")
+}
+apply(plugin = "com.google.gms.google-services")
+//
+
+ktlint {
+    debug.set(true)
+    android.set(true)
+    outputToConsole.set(true)
+    outputColorName.set("RED")
+    disabledRules.set(setOf("final-newline"))
+    reporters {
+        reporter(ReporterType.PLAIN)
+        reporter (ReporterType.CHECKSTYLE)
+        reporter (ReporterType.SARIF)
+    }
+    kotlinScriptAdditionalPaths {
+        include(fileTree("scripts/"))
+    }
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
+
+
+repositories {
+    mavenCentral()
+}
