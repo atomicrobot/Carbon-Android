e841757f6da29f4072519316d27422ffd91f4165
matthewdauterman <matthew.dauterman@atomicrobot.com>
2022-07-26 13:59:48 -0400

    John's Unit Test fix

--- app/src/test/java/com/atomicrobot/carbon/SimpleRobolectricTest.kt
+++ app/src/test/java/com/atomicrobot/carbon/SimpleRobolectricTest.kt
@@ -2,12 +2,12 @@ package com.atomicrobot.carbon
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(RobolectricTestRunner::class)
 class SimpleRobolectricTest {
 
     @Test
