b53ff0030a9191c907e057f436b6677f9a974a87
matthewdauterman <matthew.dauterman@atomicrobot.com>
2022-09-22 14:26:45 -0400

    ktlint fix

--- app/src/test/java/com/atomicrobot/carbon/data/api/github/DeepLinkInteractorTest.kt
+++ app/src/test/java/com/atomicrobot/carbon/data/api/github/DeepLinkInteractorTest.kt
@@ -9,7 +9,6 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.mockito.MockitoAnnotations
 
