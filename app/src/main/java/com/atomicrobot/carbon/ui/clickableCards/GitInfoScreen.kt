b9e886dbd977ccd73c3267a097af8706b749fb2b
matthewdauterman <matthew.dauterman@atomicrobot.com>
2023-03-13 11:31:31 -0400

    wip on vm and viewmodel

--- app/src/main/java/com/atomicrobot/carbon/ui/clickableCards/GitInfoScreen.kt
+++ app/src/main/java/com/atomicrobot/carbon/ui/clickableCards/GitInfoScreen.kt
@@ -1,24 +1,18 @@
 package com.atomicrobot.carbon.ui.main
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.atomicrobot.carbon.R
-import com.atomicrobot.carbon.data.api.github.model.Author
-import com.atomicrobot.carbon.data.api.github.model.Commit
 import com.atomicrobot.carbon.ui.components.BottomBar
 import com.atomicrobot.carbon.ui.components.TopBar
-import com.atomicrobot.carbon.ui.theme.CarbonAndroidTheme
-import com.atomicrobot.carbon.ui.theme.Purple200
-import com.atomicrobot.carbon.util.CommitPreviewProvider
 
 @Composable
 fun GitInfoNavigation(navController: NavController)
@@ -28,28 +22,28 @@ fun GitInfoNavigation(navController: NavController)
 @Composable
 @Preview
 fun GitInfoScreen(scaffoldState: ScaffoldState = rememberScaffoldState()) {
-    Scaffold(backgroundColor = Purple200) {
+    //TODO uncommenting view model causes preview to break
+//    val viewModel: GitCardInfoViewModel = getViewModel()
+    Scaffold {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TopBar("Git Info Test Title")
-            Card(
-                modifier = Modifier.fillMaxWidth()
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
                     .weight(1f)
-            ) {
-                Text(text = "Some Test Text")
-            }
+                    .padding(16.dp),
+                text = "Test text"
+            )
+//            ) {
+//                Text(text = "Some Test Text")
+//            }
             BottomBar()
         }
     }
 
-//        Text(
-//            text = commit.commitMessage,
-//            fontWeight = FontWeight.Bold,
-//            modifier = Modifier.padding(bottom = 8.dp)
-//        )
-//        Text(text = stringResource(id = R.string.author_format, commit.author))
 
 }
