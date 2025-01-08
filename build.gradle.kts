42451ff211a61d52af18d70a53ffc8ff2c9bbfe0
Kurt Burroughs <kurt@atomicrobot.com>
2025-01-08 13:31:44 -0500

    Updated project level dependencies to use version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,13 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:${Dependencies.androidPluginVersion}")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:${Dependencies.firebaseCrashlyticsGradleVersion}")
-        classpath("com.google.gms:google-services:${Dependencies.googleServicesVersion}")
-        classpath("org.jacoco:org.jacoco.core:${Dependencies.jacocoVersion}")
-        classpath("org.jetbrains.kotlin:kotlin-allopen:${Dependencies.kotlinVersion}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.kotlinVersion}")
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:${Dependencies.ktlintVersion}")
+        classpath(libs.gradle)
+        classpath(libs.firebase.crashlytics.gradle)
+        classpath(libs.google.services)
+        classpath(libs.org.jacoco.core)
+        classpath(libs.kotlin.allopen)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.ktlint.gradle)
     }
 }
 
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,8 @@
 /*The dependencies are organized in alphabetical order so they are easy to quickly locate,
 please add accordingly*/
 object Dependencies {
-    const val androidPluginVersion = "8.7.3"
     const val composeVersion = "1.7.6"
-    const val firebaseCrashlyticsGradleVersion = "3.0.2"
-    const val googleServicesVersion = "4.4.2"
     const val jacocoVersion = "0.8.11"
-    const val kotlinVersion = "2.1.0"
-    const val ktlintVersion = "12.1.2"
 }
 
 object ConfigVals {
